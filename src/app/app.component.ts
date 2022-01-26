830425f4c08bbf99fa896d5f65e857d7b29f39d2
unknown <rajputnikita5666@gmail.com>
2022-01-26 21:15:20 +0530

    Added:Event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,8 @@ export class AppComponent {
   
   //uc4
   userName: string="";
+  //uc5
+  nameError: string="";
 
   ngOnInit() {
     this.title="Hello World from Bridgelabz";
@@ -21,6 +23,18 @@ export class AppComponent {
   onClick($event:any){
     console.log("Save button is clicked!" ,$event);
     window.open(this.url, "_blank");
+  }
+
+  //uc5
+  onInput($event:any){
+    console.log("Change Event Occured!", $event.data);
+    const nameRegex=RegExp('^[A-Z]{1}[a-zA-z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect";
+
   }
 }
 
